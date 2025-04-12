e255c66767c905ce1639f5cdd25707380baf66a1
FlorinaMt <florina.mt77@gmail.com>
2025-04-12 09:41:11 +0000

    Display dummy logs to prove frontend fetches data from backend

--- api/controllers/plant_pots_controller.py
+++ api/controllers/plant_pots_controller.py
@@ -1,6 +1,7 @@
 # api/endpoints/pot.py
 
 from fastapi import APIRouter, HTTPException
+from fastapi.responses import JSONResponse
 from services.plant_pots_service import PlantPotsService
 from models.plant_pot import AddPlantPotRequest, PlantPotResponse
 
@@ -16,3 +17,21 @@ def add_plant_pot(pot: AddPlantPotRequest):
     except Exception as e:
         # Handle unexpected errors
         raise HTTPException(status_code=500, detail=str(e))
+ 
+@router.get("/logs")
+def get_logs():
+    print("Received GET /logs.")
+    try:
+        logs = [
+        "10.34 - plant watered",
+        "10.58 - window closed",
+        "18.34 - plant watered",
+    ]
+        return JSONResponse(content={"logs": logs})
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        # Handle unexpected errors
+        raise HTTPException(status_code=500, detail=str(e))
+    
+    
