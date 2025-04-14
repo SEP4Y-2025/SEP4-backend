bd7bdb1788207740592289cc4a31af44b1dcf332
FlorinaMt <florina.mt77@gmail.com>
2025-04-14 11:55:27 +0000

    Endpoints and implementations for adding and deleting plant pots

--- api/controllers/plant_pots_controller.py
+++ api/controllers/plant_pots_controller.py
@@ -3,21 +3,33 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
 from services.plant_pots_service import PlantPotsService
-from models.plant_pot import AddPlantPotRequest, PlantPotResponse
+from dto.plant_pot_dto import AddPlantPotRequest, PlantPotResponse
 
 router = APIRouter()
 
-@router.post("/pots", response_model=PlantPotResponse)
-def add_plant_pot(pot: AddPlantPotRequest):
+@router.post("/environments/{env_id}/pots", response_model=PlantPotResponse)
+def add_plant_pot(env_id : str, pot: AddPlantPotRequest):
     print("Received POST /pots with:", pot.model_dump())
     try:
-        return PlantPotsService().add_plant_pot(pot)
+        return PlantPotsService().add_plant_pot(env_id, pot)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
     except Exception as e:
         # Handle unexpected errors
         raise HTTPException(status_code=500, detail=str(e))
- 
+    
+@router.delete("/environments/{env_id}/pots/{pot_id}")
+def delete_pot(env_id : str, pot_id: str):
+    print("Received DELETE /pots/{pot_id} with id=", pot_id)
+    try:
+        if(PlantPotsService().delete_plant_pot(pot_id)):
+            return JSONResponse(content={"message": "Pot deleted successfully"}, status_code=200)
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    except Exception as e:
+        # Handle unexpected errors
+        raise HTTPException(status_code=500, detail=str(e))
+
 @router.get("/logs")
 def get_logs():
     print("Received GET /logs.")
