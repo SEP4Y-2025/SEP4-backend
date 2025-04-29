6549416f43ea8082468ba8215fc37f936b0bfc96
supendd <supendradbogati@gmail.com>
2025-04-29 09:44:01 +0200

    Update plant_types functionality: added endpoints for getting all plants from an ebv, repository methods, and service logic

--- api/controllers/plant_types_controller.py
+++ api/controllers/plant_types_controller.py
@@ -2,15 +2,16 @@
 
 from fastapi import APIRouter, HTTPException
 from services.plant_types_service import PlantTypesService
-from models.plant_type import GetAllPlantRequest, GetAllPlantResponse
 
 router = APIRouter()
 
-@router.get("/plant_types/{environment_id}", response_model=GetAllPlantResponse)
+@router.get("/plant_types/environments/{environment_id}")
 def get_all_plant_types(environment_id: str): 
     print(f"Received GET /plant-types/{environment_id}.")
     try:
-        return PlantTypesService().get_all_plant_types(environment_id)
+        service = PlantTypesService()
+        plantTypes = service.get_all_plant_types(environment_id)
+        return {"PlantTypes": plantTypes}
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
     except Exception as e:
--- services/plant_types_service.py
+++ services/plant_types_service.py
@@ -1,24 +1,17 @@
 # service/plant_types_service.py
 
-from models.plant_type import GetAllPlantRequest, GetAllPlantResponse
 from repositories.plant_types_repository import PlantTypesRepository
 from repositories.arduinos_repository import ArduinosRepository
-from core.mqtt_client import mqtt_client
 
 class PlantPotsService:
     def __init__(self):
         self.repository = PlantTypesRepository()
         self.arduinos = ArduinosRepository()
 
-    def get_all_plant_types(self, request: GetAllPlantRequest) -> GetAllPlantResponse:
-        environment_id = request.environment_id
-        
+    def get_all_plant_types(self, environment_id: str):
         plant_type_response = self.repository.get_plant_types_by_environment(environment_id)
         
         if not plant_type_response:
             raise ValueError(f"No plant types found for environment ID: {environment_id}")
         
-        
-        return GetAllPlantResponse(
-            plant_types=plant_type_response
-    )
+        return plant_type_response
