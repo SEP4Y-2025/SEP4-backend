63881ab9c23ad5e2211b036f11a3df4888af6347
Luca <Lucamouawad1@gmail.com>
2025-05-11 11:39:15 +0200

    A lot of login stuff that doesn't work yet

---
+++ middleware/jwt_middleware.py
@@ -0,0 +1,36 @@
+# This was my refference:
+# https://blog.marzeta.pl/python-and-jwt-a-comprehensive-guide-to-secure-sessions/#:~:text=Implementing%20JWTs%20with%20Python&text=In%20this%20code%2C%20we%20first,and%20the%20decoded%20JWT%20payload.
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+import jwt
+from jwt.exceptions import PyJWTError
+from services.auth_service import SECRET_KEY, ALGORITHM
+from repositories.auth_repository import AuthRepository
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        user_id = payload.get("id")
+        
+        if username is None or user_id is None:
+            raise credentials_exception
+            
+    except PyJWTError:
+        raise credentials_exception
+        
+    user = AuthRepository().find_user_by_id(user_id)
+    
+    if user is None:
+        raise credentials_exception
+        
+    return user
---
+++ repositories/auth_repository.py
@@ -0,0 +1,23 @@
+from pymongo import MongoClient
+from core.config import MONGO_URI, DB_NAME
+from bson import ObjectId
+
+class AuthRepository:
+    def __init__(self):
+        self.client = MongoClient(MONGO_URI)
+        self.db = self.client[DB_NAME]
+        self.collection = self.db["users"]
+    
+    def find_user_by_username(self, username: str):
+        user = self.collection.find_one({"username": username})
+        return user
+    
+    def find_user_by_id(self, user_id: str):
+        try:
+            return self.collection.find_one({"_id": ObjectId(user_id)})
+        except:
+            return None
+
+    def create_user(self, user_data: dict):
+        result = self.collection.insert_one(user_data)
+        return str(result.inserted_id)
